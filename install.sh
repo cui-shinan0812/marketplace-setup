9b49afec75e338da06f8e1acdc60cbdc0a7d6f1f
SHINAN <45106420+cui-shinan0812@users.noreply.github.com>
2024-09-03 19:52:55 +0900

    init

---
+++ install.sh
@@ -0,0 +1,13 @@
+/bin/bash
+sudo yum update
+sudo yum install git -y
+sudo yum install docker
+sudo systemctl start docker
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose version
+
+sudo docker run -d -p 6379:6379 redis-stack redis-server --requirepass mypassword
+git clone https://github.com/cui-shinan0812/marketplace-setup.git
+cd marketplace-setup
+sudo docker-compose up db -d
